bef6ae4fd0d7d16253b16efae487605ed481cfa9
Juho Friman <juho.t.friman@gmail.com>
2016-12-22 21:53:18 +0200

    Removed site defaults
    We want to make this INSECURE!

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [compojure "1.5.1"]
-                 [ring/ring-defaults "0.2.1"]]
+                 [ring "1.5.0"]]
   :plugins [[lein-ring "0.9.7"]]
   :ring {:handler cybersecuritybase-project-1.handler/app}
   :profiles
--- src/cybersecuritybase_project_1/handler.clj
+++ src/cybersecuritybase_project_1/handler.clj
@@ -1,11 +1,10 @@
 (ns cybersecuritybase-project-1.handler
   (:require [compojure.core :refer :all]
-            [compojure.route :as route]
-            [ring.middleware.defaults :refer [wrap-defaults site-defaults]]))
+            [compojure.route :as route]))
 
 (defroutes app-routes
   (GET "/" [] "Hello World")
   (route/not-found "Not Found"))
 
 (def app
-  (wrap-defaults app-routes site-defaults))
+  app-routes)
