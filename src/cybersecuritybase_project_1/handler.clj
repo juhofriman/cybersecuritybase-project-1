d99c77f2deb0979c3bd517d2bf5d00568959578b
Juho Friman <juho.t.friman@gmail.com>
2016-12-27 21:47:44 +0200

    Implemented REALLY basic login cookie mechanism
    It does not actually check anything. It's just a way to grab a cookie and delete it. User is interpreted to be logged in if he or she sends cookie with name ses_id.

--- src/cybersecuritybase_project_1/handler.clj
+++ src/cybersecuritybase_project_1/handler.clj
@@ -1,13 +1,36 @@
 (ns cybersecuritybase-project-1.handler
   (:require [compojure.core :refer :all]
             [compojure.route :as route]
+            [ring.middleware.cookies :refer [wrap-cookies]]
             [net.cgrand.enlive-html :as html]))
 
-(html/deftemplate main-template "templates/main.html" [])
+(defn authenticate [username password]
+  ; actually this could return login hash or nil
+  true)
+
+(html/deftemplate login-template "templates/login.html"
+  []
+  [:head :title] (html/content "Please login")
+  [:body :h1] (html/content (str "Please login ")))
+
+(html/deftemplate main-template "templates/main.html"
+  []
+  [:head :title] (html/content "Application name")
+  [:body :h1] (html/content (str "Nice, you're in")))
 
 (defroutes app-routes
-  (GET "/" [] (main-template))
+  (GET "/" {cookies :cookies}
+       (if (contains? cookies "ses_id")
+         (main-template)
+         (login-template)))
+  (POST "/login.html" [username password]
+        (if (authenticate username password)
+          {:status 302 :headers {"Location" "/"} :cookies {"ses_id" {:value "something"}}  :body ""}
+          {:status 302 :headers {"Location" "/?error=invalid-credentials"} :body ""}))
+  (POST "/logout.html" []
+        {:status 302 :headers {"Location" "/"} :cookies { "ses_id" {:value "" :max-age -1}}})
   (route/not-found "Not Found"))
 
 (def app
-  app-routes)
+  (-> app-routes
+      wrap-cookies))
--- test/cybersecuritybase_project_1/handler_test.clj
+++ test/cybersecuritybase_project_1/handler_test.clj
@@ -3,7 +3,25 @@
             [ring.mock.request :as mock]
             [cybersecuritybase-project-1.handler :refer :all]))
 
+(defn sets-cookie [response name]
+  (.contains (clojure.string/join "&" (get-in response [:headers "Set-Cookie"] [])) name))
+
 (deftest test-app
+  (testing "login should redirect and set cookie for succesfull login"
+    (with-redefs-fn {#'cybersecuritybase-project-1.handler/authenticate (constantly true)}
+      #(let [response (app (-> (mock/request :post "/login.html")
+                               (mock/body {"username" "foo" "password" "bar"})))]
+        (is (= (:status response) 302))
+        (is (sets-cookie response "ses_id")))))
+  (testing "login should redirect for invalid credentials"
+    (with-redefs-fn {#'cybersecuritybase-project-1.handler/authenticate (constantly false)}
+      #(let [response (app (-> (mock/request :post "/login.html")
+                               (mock/body {"username" "foo" "password" "bar"})))]
+        (is (= (:status response) 302)))))
+  (testing "logout should expire cookie"
+    (let [response (app (-> (mock/request :post "/logout.html")))]
+      (is (= (:status response) 302))
+      (is (sets-cookie response "ses_id"))))
   (testing "main route"
     (let [response (app (mock/request :get "/"))]
       (is (= (:status response) 200))))
